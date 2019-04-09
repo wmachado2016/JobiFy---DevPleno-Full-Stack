147a0cc73ed4ea08dc5acd502b11bef144145265
wanderley <2008.wsm@gmail>
2019-04-09 11:05:22 -0300

    adicionando path as pastas

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
-const dbConection = sqlite.open('banco.sqlite', {Promise})
-
+const dbConection = sqlite.open(path.resolve(__dirname,'banco.sqlite'), {Promise})
+const path= require('path')
 const port = process.env.PORT || 3000
 
+app.set('views',path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/',async (request, response) => { 
