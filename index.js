76d90a09b39150aeaf1339b57c02c95c9d083284
wanderley <2008.wsm@gmail>
2019-04-09 10:32:52 -0300

    adição da port

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
 const dbConection = sqlite.open('banco.sqlite', {Promise})
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:true}))
@@ -97,7 +99,7 @@ const init = async() => {
 
 init()
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if(err){
         console.log('Erro ao conectar ao servidor.')
     }
