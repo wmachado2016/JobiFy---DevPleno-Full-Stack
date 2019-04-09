368c4895a65c917776bccc470993cf456221e825
wanderley <2008.wsm@gmail>
2019-04-09 11:30:41 -0300

    atualizando caminhos path do zeit no index.js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+
+const path = require('path')
+
 const sqlite = require('sqlite')
-const dbConection = sqlite.open(path.resolve(__dirname,'banco.sqlite'), {Promise})
-const path= require('path')
+const dbConection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise })
+
 const port = process.env.PORT || 3000
 
-app.set('views',path.join(__dirname,'views'))
+app.use('/admin', (req, res, next) => {
+    if(req.hostname === 'localhost'){
+        next()
+    }else{
+        res.send('Not allowed')
+    }
+})
+
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname,'public')))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/',async (request, response) => { 
     const db = await dbConection
