551943450e786f9b71f50aaafda4eca2d8937cb0
wanderley <2008.wsm@gmail>
2019-04-10 00:04:43 -0300

    corrigindo segursnça de acesso ao admin.

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ const path = require('path')
 const sqlite = require('sqlite')
 const dbConection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise })
 
-const port = process.env.PORT || 3000
-
-/*app.use('/admin', (req, res, next) => {
+const port = process.env.PORT || 3000app.use('/admin', (req, res, next) => {
     if(req.hostname === 'localhost'){
         next()
     }else{
         res.send('Not allowed')
     }
-})*/
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
